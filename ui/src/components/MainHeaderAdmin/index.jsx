ef2454aca5b16f3b541bd2c6958f58643f33dfca
Rafael606 <rafaelcamposdeoliveira56@gmail.com>
2024-07-03 21:10:58 -0300

    add Profile

--- ui/src/components/MainHeaderAdmin/index.jsx
+++ ui/src/components/MainHeaderAdmin/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, Profile, NomePerfil, Perfil, LogoInst} from "./styles" 
+import React, { useState, useRef, useEffect } from "react";
+import { Container, Profile, NomePerfil, LogoInst, RelativeContainer, UserImg, Menu, MenuItem,CardPerfil} from "./styles" 
 // Importante as estilizações do CSS;
 
 import photoProfile from "../../assets/mode-portrait.svg" 
@@ -9,17 +9,58 @@ import logo from "../../assets/logoInstituto.png"
 // Importanto a logo do Instituto Energisa;
 
 const MainHeader = () => {
+    const [open, setOpen] = useState(false);
+    const Menus = ['Meus Dados', 'Redefinir Senha', 'Sair'];
+
+    const menuRef = useRef();
+    const imgRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (e.target !== menuRef.current && e.target !== imgRef.current) {
+                setOpen(false);
+            }
+        };
+
+        window.addEventListener('click', handleClickOutside);
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
     return (
         <Container> 
             <LogoInst src={logo} alt="Logo Insituto Energisa" />
             {/* Logo Instituto Energisa */}
 
             <Profile>
-                <Perfil src={photoProfile} alt="Foto de Perfil" />
+            <RelativeContainer>
+                <CardPerfil>
+                <UserImg 
+                    ref={imgRef}
+                    onClick={() => setOpen(!open)}
+                    src={photoProfile}
+                    alt="Foto de Perfil"
+                />
                 <NomePerfil href="#">
-                    Administrador
+                    Admin
                 </NomePerfil>
-            </Profile>
+                </CardPerfil>
+                {open && (
+                    <Menu ref={menuRef}>
+                        <ul>
+                            {Menus.map((menu) => (
+                                <MenuItem 
+                                    onClick={() => setOpen(false)}
+                                    key={menu}
+                                >
+                                    {menu}
+                                </MenuItem>
+                            ))}
+                        </ul>
+                    </Menu>
+                )}
+            </RelativeContainer>
+        </Profile>
         </Container>
     );
 }
--- ui/src/components/MainHeaderAdmin/styles.js
+++ ui/src/components/MainHeaderAdmin/styles.js
@@ -12,29 +12,75 @@ export const Container = styled.div`
      border-bottom: 3px solid  #3D978F;
 `;
 
-export const Profile = styled.div`
-     display: flex;
+export const CardPerfil = styled.div`
+    display: flex;
 `;
 
-export const NomePerfil = styled.a`
+export const Profile = styled.div`
+     padding-top: 4rem; 
+     display: flex;
+     align-items: center;
+`;
+
+export const NomePerfil = styled.div`
+     padding-top: 5rem;
+     /* margin-top: 10rem; */
      text-decoration: none;
-     margin-right: 10px;
+     margin-right: 15px;
      color: #3d978f;
 
      transition: opacity .3s;
 
      &:hover {
           opacity: .7;
-     };
-
+     }
 `;
 
-export const Perfil = styled.img`
-    height: 20px;
-    width: 20px;
-    margin-right: 10px;
-`;
+// export const Perfil = styled.img`
+//     height: 20px;
+//     width: 20px;
+//     margin-right: 10px;
+// `;
 
-export const LogoInst= styled.img`
+export const LogoInst = styled.img`
     height: 60px;
+`;
+
+export const RelativeContainer = styled.div`
+    position: relative;
+    display: flex;
+    
+`;
+
+export const UserImg = styled.img`
+    margin-bottom: 2.7rem;
+    height: 2rem; 
+    width: 2rem; 
+    object-fit: cover;
+    /* border: 4px solid #9CA3AF;  */
+    /* border-radius: 9999px;  */
+    cursor: pointer;
+`;
+
+export const Menu = styled.div`
+    border-radius: 20px;
+    background-color: #FFFFFF; 
+    padding: 1rem; 
+    width: 9rem; 
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); 
+    position: absolute;
+    left: -2.9rem; 
+    top: 3.3rem; 
+`;
+
+export const MenuItem = styled.li`
+    list-style-type: none;
+    padding: 0.5rem; 
+    font-size: 1.125rem; 
+    cursor: pointer;
+    border-radius: 0.375rem; 
+    
+    &:hover {
+        background-color: #BFDBFE; 
+    }
 `;
