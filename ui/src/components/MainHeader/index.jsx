67af795db4915a8725a8871b552c3e02248c5442
maryangelasoares <maryangelasoares@hotmail.com>
2024-07-09 16:56:17 -0300

    Update Perfil Ui

--- ui/src/components/MainHeader/index.jsx
+++ ui/src/components/MainHeader/index.jsx
@@ -1,24 +1,60 @@
-import React from "react";
-import { Container, Profile, NomePerfil, Perfil, LogoInst} from "./styles" 
-// Importante as estilizações do CSS;
+import React, { useState, useRef, useEffect } from "react";
+import { Container, Profile,  LogoInst, RelativeContainer, UserImg, Menu, MenuItem, CardPerfil } from "./styles"
 
-import photoProfile from "../../assets/mode-portrait.svg" 
-//Importando a foto de perfil do Usuário;
-
+import photoProfile from "../../assets/mode-portrait.svg"
 import logo from "../../assets/logoInstituto.png"
-// Importanto a logo do Instituto Energisa;
 
 const MainHeader = () => {
+    const [open, setOpen] = useState(false);
+    const Menus = ['NomeUsuário', 'Meus Dados', 'Redefinir Senha', 'Sair'];
+
+    const menuRef = useRef();
+    const imgRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (e) => {
+            if (e.target !== menuRef.current && e.target !== imgRef.current) {
+                setOpen(false);
+            }
+        };
+
+        window.addEventListener('click', handleClickOutside);
+        return () => {
+            window.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <Container> 
+        <Container>
             <LogoInst src={logo} alt="Logo Insituto Energisa" />
-            {/* Logo Instituto Energisa */}
 
             <Profile>
-                <Perfil src={photoProfile} alt="Foto de Perfil" />
-                <NomePerfil href="#">
-                    Usuário
-                </NomePerfil>
+                <RelativeContainer>
+
+                    <CardPerfil>
+                        <UserImg
+                            ref={imgRef}
+                            onClick={() => setOpen(!open)}
+                            src={photoProfile}
+                            alt="Foto de Perfil"
+                        />
+                    </CardPerfil>
+                    {open && (
+                        <Menu ref={menuRef}>
+                            <ul>
+                                {Menus.map((menu) => (
+                                    <MenuItem
+                                        onClick={() => setOpen(false)}
+                                        key={menu}
+                                    >
+                                        {menu}
+                                    </MenuItem>
+                                ))}
+                            </ul>
+                        </Menu>
+                    )}
+
+                </RelativeContainer>
             </Profile>
         </Container>
     );
