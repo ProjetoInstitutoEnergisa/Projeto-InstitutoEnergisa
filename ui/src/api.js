65b2d68e897f310bfb57bf192c904a770436786c
maryangelasoares <maryangelasoares@hotmail.com>
2024-07-09 02:11:05 -0300

    Update

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/projetosAcoes', // Substitua pelo URL do seu servidor
+    baseURL: 'http://localhost:3000/api/projetosAcoes',
 });
+//instancia login
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
 
 api.interceptors.response.use(
     response => response.data,
